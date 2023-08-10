1f57cf178b199741849f4434f4ac336f0b92ced3
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-10 04:48:04 +0000

    feat: generouted

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById("app")!;
+createRoot(container).render(<Routes />);
