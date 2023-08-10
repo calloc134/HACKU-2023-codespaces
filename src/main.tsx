a2207a670122fc2fa1fb0477c4867da3ae374f28
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-10 09:09:35 +0000

    feat: 最低限UIを調整

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { Routes } from "@generouted/react-router";
-import "./index.css";
 
 const container = document.getElementById("app")!;
 createRoot(container).render(<Routes />);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Box, Flex, Heading, Button, Text } from "@chakra-ui/react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const Home = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      bg="teal.500"
+      minH="100vh"
+      p={8}
+    >
+      <Box
+        textAlign="center"
+        mb={6}
+        p={6}
+        bg="gray.100"
+        borderRadius="md"
+        shadow="xl"
+        color="white"
+      >
+        <Heading mb={4} color={"gray.600"}>
+          usotsukey
+        </Heading>
+        <Text color={"gray.600"}>嘘しかつけないSNS</Text>
+        <Button
+          colorScheme="whiteAlpha"
+          size="lg"
+          mt={4}
+          background={"gray.600"}
+        >
+          ログイン画面へ
+        </Button>
+      </Box>
+    </Flex>
   );
-}
+};
 
-export default App;
+export default Home;
