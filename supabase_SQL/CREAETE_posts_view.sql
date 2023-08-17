624e575c99cbbc0bc105e70ef11f5938281a3138
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-08-17 17:11:53 +0900

    docs: SQLクエリ文を移動

--- supabase_SQL/CREAETE_posts_view.sql
+++ supabase_SQL/CREAETE_posts_view.sql
@@ -3,14 +3,16 @@
 
 create view post_view as
 select
-  posts.id,
+  posts.post_id,
   posts.auth_id,
   posts.created_at,
+  posts.updated_at,
+  posts.is_active,
   posts.content,
   posts.is_lie,
   count(likes.post_id) as likes_num,
   count(val_lies.post_id) as val_lie_num
 from posts
-left join likes on posts.id = likes.post_id
-left join val_lies on posts.id = val_lies.post_id
-group by posts.id;
+left join likes on posts.post_id = likes.post_id
+left join val_lies on posts.post_id = val_lies.post_id
+group by posts.post_id;
