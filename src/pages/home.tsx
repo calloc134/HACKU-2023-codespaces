b9be73d9002a0605b66d25f4dc871bfa5763abec
はる <115918416+haruto0707@users.noreply.github.com>
2023-08-18 07:35:59 +0000

    feat: homeページのヘッダーを作成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,37 +1,41 @@
 import { Button, Flex, StackDivider, VStack } from "@chakra-ui/react";
 import { PostCard } from "../components/home/PostCard";
+import { WithAction } from "../components/home/Header";
 
 export const HomePage = () => {
   return (
-    <Flex
-      direction="column"
-      align="center"
-      justify="center"
-      bg="#D6BCFA"
-      minH="100vh"
-      p={8}
-    >
-      <Button>投稿</Button>
-      <VStack
-        bg="white"
-        marginLeft="auto"
-        marginRight="auto"
-        border="gray.200"
-        divider={<StackDivider borderColor="gray.200" />}
-        maxW="70vw"
-        minW="70vw"
+    <>
+      <WithAction />
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+        bg="#D6BCFA"
         minH="100vh"
+        p={8}
       >
-        <PostCard content="a" />
-        <PostCard content="b" />
-        <PostCard content="c" />
-        <PostCard content="a" />
-        <PostCard content="b" />
-        <PostCard content="c" />
-        <PostCard content="a" />
-        <PostCard content="b" />
-        <PostCard content="c" />
-      </VStack>
-    </Flex>
+        <Button>投稿</Button>
+        <VStack
+          bg="white"
+          marginLeft="auto"
+          marginRight="auto"
+          border="gray.200"
+          divider={<StackDivider borderColor="gray.200" />}
+          maxW="70vw"
+          minW="70vw"
+          minH="100vh"
+        >
+          <PostCard content="a" />
+          <PostCard content="b" />
+          <PostCard content="c" />
+          <PostCard content="a" />
+          <PostCard content="b" />
+          <PostCard content="c" />
+          <PostCard content="a" />
+          <PostCard content="b" />
+          <PostCard content="c" />
+        </VStack>
+      </Flex>
+    </>
   );
 };
