97f6927b9591ebf898212cec65b88129fd8572b4
ibukani <127748974+ibukani@users.noreply.github.com>
2023-08-19 02:50:29 +0000

    feat: supabase.tsxに関数を追加

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 
 const supabase = createClient("", "");
 
+/**
+ * セッションの状態を取得する。
+ *
+ * @returns {Session | null}
+ */
 const useSession = () => {
   const [session, setSession] = useState<Session | null>(null);
 
@@ -23,4 +28,38 @@ const useSession = () => {
   });
 
   return session;
+};
+
+/**
+ * すべての投稿のうち，作成日時が新しい100件を取得する
+ * @returns
+ */
+export const fetchPosts = async () => {
+  const { data, error } = await supabase
+    .from("posts_view")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .limit(100);
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+/**
+ * 嘘を投稿する
+ * @param content 嘘の中身
+ */
+export const sendPost = async (content: string) => {
+  // ログイン中のユーザーidを取得
+  const user_data = await supabase.auth.getUser();
+  const auth_user_id = user_data.data.user.id;
+
+  // postsテーブルにレコードを追加する
+  const { error } = await supabase
+    .from("posts")
+    .insert({ auth_id: auth_user_id, content: content });
+  // 嘘判定の結果をどうするかはまだ未定のため省略
 };
