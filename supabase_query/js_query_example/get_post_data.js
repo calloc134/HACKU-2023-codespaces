adf858240823cd3d4cfe1338093acf463c6da11f
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-08-20 13:15:05 +0900

    docs: いいね処理しやすいように更新 サンプルクエリ更新

--- supabase_query/js_query_example/get_post_data.js
+++ supabase_query/js_query_example/get_post_data.js
@@ -3,16 +3,46 @@ const user_data = await supabase.auth.getUser();
 const auth_user_id = user_data.data.user.id;
 
 
-// ログイン中のユーザーの投稿を取得する
-const { error } = await supabase
-    .from('posts_view')
-    .select('*')
-    .eq('auth_id', auth_user_id);
-
 
 // すべての投稿のうち，作成日時が新しい100件を取得する
-const { data, error } = await supabase
-    .from('posts_view')
-    .select('*')
-    .order('created_at', { ascending: false })
-    .limit(100);
+const select_user_posts = async () => {
+    // ログイン中のユーザーidを取得
+    const user_data = await supabase.auth.getUser(); 
+    const auth_user_id = user_data.data.user.id;
+
+
+    // すべての投稿のうち，作成日時が新しい100件を取得する
+    const { data, error } = await supabase.rpc(
+        'get_posts', { my_auth_id: auth_user_id }
+        )
+        .order('created_at', { ascending: false })
+        .limit(100);
+
+    if (error) {
+        // ここでエラーハンドリング
+        alert(error.message)
+    } else {
+        // デバッグ用に表示
+        console.log(data);
+    }
+}
+
+
+// 返ってくるデータの例
+[
+    {
+        post_id: 2, //bigint
+        auth_id: "0b117dc0-86ad-460f-9d19-33689b467bcf", //uuid
+        content: "なにかしらのテキスト", //text
+        created_at: "2023-08-20T03:18:08.492813+00:00", //timestamp with time zone
+        updated_at: "2023-08-20T03:18:08.492813+00:00", //timestamp with time zone
+        is_active: null, //boolean
+        is_lie: null, //boolean
+        name: "YourName", //text
+        app_user_id: null, // text
+        likes_num: 0, // bigint
+        liked: false, //boolean 自分がその投稿にいいねしたかどうか
+        val_lies_num: 0, //bigint
+        val_liked: false //boolean 自分がその投稿にだまされたをつけたかどうか
+    }
+]
--- supabase_query/test_sql/view.sql
+++ supabase_query/test_sql/view.sql
@@ -27,3 +27,58 @@ SELECT
 
 FROM posts p
 JOIN app_users u ON p.auth_id = u.auth_id;
+
+
+
+
+-----------------------------------
+
+
+SELECT posts.*, 
+    app_users.name,
+    app_users.app_user_id,
+    (
+        SELECT COUNT(*) 
+        FROM likes l 
+        WHERE l.post_id = p.post_id
+    ) AS total_likes,
+    CASE 
+        WHEN likes.auth_id IS NOT NULL 
+        THEN true 
+        ELSE false 
+    END AS liked
+FROM posts
+LEFT JOIN likes ON posts.post_id = likes.post_id AND likes.auth_id = :my_user_id
+JOIN app_users ON posts.auth_id = app_users.auth_id;
+
+---------------------------
+
+
+CREATE OR REPLACE FUNCTION get_posts(my_user_id INT)
+RETURNS TABLE (
+    post_id INT,
+    name TEXT,
+    app_user_id INT,
+    likes_num INT,
+    liked BOOLEAN
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT posts.post_id,
+           app_users.name,
+           app_users.app_user_id,
+           (
+               SELECT COUNT(*) 
+               FROM likes l 
+               WHERE l.post_id = posts.post_id
+           ) AS total_likes,
+           CASE 
+               WHEN likes.auth_id IS NOT NULL 
+               THEN true 
+               ELSE false 
+           END AS liked
+    FROM posts
+    LEFT JOIN likes ON posts.post_id = likes.post_id AND likes.auth_id = my_user_id
+    JOIN app_users ON posts.auth_id = app_users.auth_id;
+END;
+$$ LANGUAGE plpgsql;
