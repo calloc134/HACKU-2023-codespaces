64cc3947f6137698f85ee2226e351aba01a1c94e
はる <115918416+haruto0707@users.noreply.github.com>
2023-08-22 12:49:05 +0000

    fix: postや画面遷移した後にリクエストがすごいことになる不具合を修正

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,19 @@
-import { createClient, Session } from "@supabase/supabase-js";
+import { createClient, Session, User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 export const supabase = createClient(
   "https://rphpgdwfvgbqodprwhbo.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwaHBnZHdmdmdicW9kcHJ3aGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE4NDU4NTEsImV4cCI6MjAwNzQyMTg1MX0.Rni5vkoj06n16aobQ3uuz-Id2A09a4GEPCx6EkzZLgs",
 );
+
+let cachedUserData: User | null = null;
+
+const getUser = async () => {
+  if (cachedUserData) return cachedUserData;
+  const user_data = await supabase.auth.getUser();
+  cachedUserData = user_data.data.user;
+  return cachedUserData;
+};
 
 export const useSession = () => {
   const [session, setSession] = useState<Session | null>(null);
@@ -28,36 +37,26 @@ export const useSession = () => {
   return session;
 };
 
-/**
- * SignUp時にユーザー名も登録するようにする関数
- * @param signUpData SignUpに必要な情報
- * @param username ユーザー名
- * @returns 成功時 trueを返す。 失敗時は falseを返す
- */
 export const signUpAndUpdateUsername = async (
   signUpData: { email: string; password: string },
   app_user_id: string,
   username: string,
 ) => {
   try {
-    // 登録処理
     const signupQuery = await supabase.auth.signUp(signUpData);
 
-    // エラーの確認
     if (signupQuery.error) {
       throw signupQuery.error;
     }
 
-    // エラーなしでユーザー名前を登録するように設定
-    const user = await supabase.auth.getUser(); // ログイン中のユーザー情報を取得
+    const user = await getUser();
 
-    if (user.data.user == null) {
+    if (user == null) {
       alert("エラー(USER_IS_NULL)");
       return false;
     }
 
-    const auth_user_id = user.data.user.id;
-    // アプリ側のユーザテーブルのユーザ名を更新する
+    const auth_user_id = user.id;
     const usernameQuery = await supabase
       .from("app_users")
       .update({ name: username, app_user_id: app_user_id })
@@ -74,21 +73,16 @@ export const signUpAndUpdateUsername = async (
   }
 };
 
-/**
- * すべての投稿のうち，作成日時が新しい100件を取得する
- * @returns
- */
 export const fetchPosts = async () => {
   try {
-    // ログイン中のユーザーidを取得
-    const user_data = await supabase.auth.getUser();
+    const user = await getUser();
 
-    if (!user_data.data.user) {
+    if (!user) {
       alert("エラー(USER_IS_NULL)");
       return;
     }
 
-    const auth_user_id = user_data.data.user.id;
+    const auth_user_id = user.id;
 
     // すべての投稿のうち，作成日時が新しい100件を取得する
     const { data, error } = await supabase
@@ -97,7 +91,6 @@ export const fetchPosts = async () => {
       .limit(100);
 
     if (error) {
-      // ここでエラーハンドリング
       alert(error.message);
     } else {
       return data;
@@ -107,21 +100,14 @@ export const fetchPosts = async () => {
   }
 };
 
-/**
- * 嘘を投稿する
- * @param content 嘘の中身
- */
 export const sendPost = async (content: string) => {
   try {
-    // ログイン中のユーザーidを取得
-    const user_data = await supabase.auth.getUser();
+    const user = await getUser();
 
-    // null check
-    if (user_data.data.user == null) return;
+    if (user == null) return;
 
-    const auth_user_id = user_data.data.user.id;
+    const auth_user_id = user.id;
 
-    // postsテーブルにレコードを追加する
     const { error } = await supabase
       .from("posts")
       .insert({ auth_id: auth_user_id, content: content });
